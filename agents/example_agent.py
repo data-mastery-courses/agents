459d86f0bd3f67d975e3f3c177d6d607f8e3fc4b
Simon Riezebos <info@datagiant.org>
2025-05-01 16:23:34 +0200

    Add dotenv

--- agents/example_agent.py
+++ agents/example_agent.py
@@ -1,6 +1,9 @@
 from agno.agent import Agent
 from agno.models.google import Gemini
 from agno.tools.duckduckgo import DuckDuckGoTools
+from dotenv import load_dotenv
+
+load_dotenv()
 
 agent = Agent(
     model=Gemini(id="gemini-2.5-flash-preview-04-17"),
